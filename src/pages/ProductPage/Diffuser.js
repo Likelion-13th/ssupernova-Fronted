79a2a66a335206cb07ea81d3974dcc81eebc5c9a
csy5271abcd <csy5271abcd@gmail.com>
2025-03-27 20:19:08 +0900

    250327 frontent

---
+++ src/pages/ProductPage/Diffuser.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Diffuser=()=>{
+    return(
+        <div>
+            <h1>This is Diffuser Page</h1>
+            <p>이 페이지는 다양한 디퓨저 상품을 살펴볼 수 있는 페이지입니다~!!</p>
+        </div>
+    )
+}
+
+export default Diffuser;
---
+++ src/pages/ProductPage/New.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const New=()=>{
+    return(
+        <div>
+            <h1>This is New Page</h1>
+            <p>이 페이지는 최근 출시된 상품들을 볼 수 있는 페이지입니다~!!</p>
+        </div>
+    )
+}
+
+export default New;
