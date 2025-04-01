16818d8a6a4bb264b670f8dfb1074c5ad5f90e4a
csy5271abcd <csy5271abcd@gmail.com>
2025-04-01 12:11:29 +0900

    add _redirects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Footer from "./components/Footer";
 import Home from './pages/Home/Home';
 import Mypage from './pages/Mypage/Mypage';
 import Diffuser from './pages/ProductPage/Diffuser';
@@ -11,12 +12,14 @@ function App() {
   return (
     <Router>
       <Routes>
+        
         <Route path="/" element={<Home />} />
         <Route path="/mypage" element={<Mypage />} />
         <Route path="/diffuser" element={<Diffuser />} />
         <Route path="/perfume" element={<Perfume />} />
         <Route path="/new" element={<New />} />
       </Routes>
+      <Footer/>
     </Router>
   );
 }
