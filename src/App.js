3b5929ed64a1599c59ebb2a9927b38da7d2accda
csy5271abcd <csy5271abcd@gmail.com>
2025-04-03 19:18:11 +0900

    Header Answer(25/04/03)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Footer from "./components/Footer";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Home from './pages/Home/Home';
-import Mypage from './pages/Mypage/Mypage';
-import Diffuser from './pages/ProductPage/Diffuser';
-import Perfume from './pages/ProductPage/Perfume';
-import New from './pages/ProductPage/New';
+import Home from "./pages/Home/Home";
+import Mypage from "./pages/Mypage/Mypage";
+import New from "./pages/ProductPage/New";
+import Perfume from "./pages/ProductPage/Perfume";
+import Diffuser from "./pages/ProductPage/Diffuser";
 
 function App() {
   return (
@@ -16,11 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mypage" element={<Mypage />} />
-        <Route path="/diffuser" element={<Diffuser />} />
-        <Route path="/perfume" element={<Perfume />} />
         <Route path="/new" element={<New />} />
+        <Route path="/perfume" element={<Perfume />} />
+        <Route path="/diffuser" element={<Diffuser />} />
       </Routes>
-      <Footer />
     </Router>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,42 @@
-
-import React from 'react';
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import"../styles/Header.css";
+import "../styles/Header.css";
 
 const Header = () => {
-    const location = useLocation();
-    const currentPage = location.pathname;
-    return(
-        <div className={`header-container ${currentPage === "/mypage" ? "mypage-bg" : ""}`}>
-            <Link to="/" className={currentPage === "/" ? "active-home" : ""}> LIKELION </Link>
-            <div className="header-subtitle-wrapper">
-                <Link to="/new" className={currentPage === "/new" ? "active" : ""}> New </Link>
-                <Link to="/perfume" className={currentPage === "/perfume" ? "active" : ""}> Perfume </Link>
-                <Link to="/diffuser" className={currentPage === "/diffuser" ? "active" : ""}> Diffuser </Link>
-                <Link to="/mypage" className={currentPage === "/mypage" ? "active-bg" : ""}> Mypage </Link>
-            </div>
-        </div>
-    );
-};
+  const location = useLocation();
+  const currentPage = location.pathname;
+  const isMyPage = location.pathname === "/mypage";
 
+  return (
+    <header data-page={isMyPage ? "mypage" : ""}>
+      <div className="logo">
+        <Link to="/">LIKELION</Link>
+      </div>
+      <nav className="nav-menu">
+        <Link to="/new" className={currentPage === "/new" ? "active" : ""}>
+          New
+        </Link>
+        <Link
+          to="/perfume"
+          className={currentPage === "/perfume" ? "active" : ""}
+        >
+          Perfume
+        </Link>
+        <Link
+          to="/diffuser"
+          className={currentPage === "/diffuser" ? "active" : ""}
+        >
+          Diffuser
+        </Link>
+        <Link
+          to="/mypage"
+          className={currentPage === "/mypage" ? "active" : ""}
+        >
+          Mypage
+        </Link>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
